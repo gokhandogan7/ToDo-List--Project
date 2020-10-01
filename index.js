2bde2ce074a0c1d4c2879a0e860642f1d0b6694f
gokhan <gdogan3746@gmail.com>
2020-10-01 21:34:32 +0200

    displayTodos function has created

--- index.js
+++ index.js
@@ -19,3 +19,23 @@ function doneTodo(todoId) {
 
   displayTodos();
 }
+
+function displayTodos(){
+
+    todoList.forEach((item) => {
+       const todoElement = document.createElement("li");
+        
+       todoElement.innerText = item.todoText;
+       todoElement.setAttribute("data-id", item.id)
+       if (item.isDone){
+           listElement.classList.add("checked")
+       } 
+
+       listElement.addEventListener("click", function(e){
+            const selectedId = e.target.getattribute("data-id");
+            doneTodo(selectedId);
+       })
+       
+       todoListElement.appendChild(listElement);
+    });
+}
