333a803a841de53c369dbd65630b6c4fa7890a97
gokhan <gdogan3746@gmail.com>
2020-10-02 22:02:01 +0200

    add line through to the checked list elements

--- index.js
+++ index.js
@@ -1,53 +1,112 @@
-// TO-DO
+const todoList = [];
+
 const todoListElement = document.querySelector("#myUL");
 
 document.querySelector("#todo_button").addEventListener("click", addTodo);
+
 function addTodo() {
   const todoText = document.querySelector("#myInput").value;
 
   const todoObject = {
-    id: todoListElement.length,
+    // id: todoList.length * 5,
+    id: todoList.length * 5,
     todoText: todoText,
     isDone: false,
   };
+
   todoList.push(todoObject);
+  displayTodos();
 }
 
 function doneTodo(todoId) {
-  const selectedTodoId = todoList.findIndex((myTodo) => myTodo.id == todoId);
+  const selectedTodoIndex = todoList.findIndex((item) => item.id == todoId);
   todoList[selectedTodoIndex].isDone = true;
 
   displayTodos();
 }
 
 function displayTodos() {
-    todoListElement.innerHTML = "";
-    document.querySelector("#myInput").value = "";
-  
-    todoList.forEach((item) => {
-      const listElement = document.createElement("li");
-  
-      listElement.innerText = item.todoText;
-      listElement.setAttribute("data-id", item.id);
-  
-      // if (item.isDone) {
-      //   listElement.classList.add("checked");
-      // }
-  
-      listElement.addEventListener("click", function (e) {
-        const selectedId = e.target.getAttribute("data-id");
-      //   e.target.style.textDecoration = "line-through"
-      //   console.log(e.target)
-      //   doneTodo(selectedId);
-          if (e.target.style.textDecoration == "line-through"){
-              e.target.style.textDecoration = "none"
-          } else{
-              e.target.style.textDecoration = "line-through"
-          }
-          
-      
-      });
-  
-      todoListElement.appendChild(listElement);
+  todoListElement.innerHTML = "";
+  document.querySelector("#myInput").value = "";
+
+  todoList.forEach((item) => {
+    const listElement = document.createElement("li");
+
+    listElement.innerText = item.todoText;
+    listElement.setAttribute("data-id", item.id);
+    // listElement.dataset.id=item.id
+    // if (item.isDone) {
+    //   listElement.classList.add("checked");
+    // }
+
+    
+
+    listElement.addEventListener("click", function (e) {
+      const selectedId = e.target.getAttribute("data-id");
+        // e.target.style.textDecoration = "line-through"
+        // doneTodo(selectedId);
+        
+      if (e.target.style.textDecoration == "line-through") {
+        e.target.style.textDecoration = "none";
+      } else {
+        e.target.style.textDecoration = "line-through";
+      }
     });
-  }
+    
+      
+    todoListElement.appendChild(listElement);
+  });
+}
+
+// // TO-DO
+// const todoListElement = document.querySelector("#myUL");
+
+// document.querySelector("#todo_button").addEventListener("click", addTodo);
+// function addTodo() {
+//   const todoText = document.querySelector("#myInput").value;
+
+//   const todoObject = {
+//     id: todoListElement.length,
+//     todoText: todoText,
+//     isDone: false,
+//   };
+//   todoList.push(todoObject);
+// }
+
+// function doneTodo(todoId) {
+//   const selectedTodoId = todoList.findIndex((myTodo) => myTodo.id == todoId);
+//   todoList[selectedTodoIndex].isDone = true;
+
+//   displayTodos();
+// }
+
+// function displayTodos() {
+//     todoListElement.innerHTML = "";
+//     document.querySelector("#myInput").value = "";
+
+//     todoList.forEach((item) => {
+//       const listElement = document.createElement("li");
+
+//       listElement.innerText = item.todoText;
+//       listElement.setAttribute("data-id", item.id);
+
+//       // if (item.isDone) {
+//       //   listElement.classList.add("checked");
+//       // }
+
+//       listElement.addEventListener("click", function (e) {
+//         const selectedId = e.target.getAttribute("data-id");
+//       //   e.target.style.textDecoration = "line-through"
+//       //   console.log(e.target)
+//       //   doneTodo(selectedId);
+//           if (e.target.style.textDecoration == "line-through"){
+//               e.target.style.textDecoration = "none"
+//           } else{
+//               e.target.style.textDecoration = "line-through"
+//           }
+
+//       });
+
+//       todoListElement.appendChild(listElement);
+//     });
+//   }
